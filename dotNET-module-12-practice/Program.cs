8fabe21611c3536ea0c5f164669c6a7d3fdef76d
Olzhas Adylkhanov <adylkh4nov@gmail.com>
2023-11-26 17:14:47 +0600

    Добавьте файлы проекта.

---
+++ dotNET-module-12-practice/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET_module_12_practice
+{
+
+    public interface IPropertyChanged
+    {
+        event PropertyEventHandler PropertyChanged;
+    }
+
+    public delegate void PropertyEventHandler(object sender, PropertyChangedEventArgs e);
+
+    public class PropertyChangedEventArgs : EventArgs
+    {
+        public string PropertyName { get; }
+
+        public PropertyChangedEventArgs(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+    }
+
+    public class MyClass : IPropertyChanged
+    {
+        private string _name;
+
+        public event PropertyEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+
+            obj.PropertyChanged += (sender, e) =>
+            {
+                Console.WriteLine($"Property {e.PropertyName} has changed.");
+            };
+
+            obj.Name = "NewName";
+
+            Console.ReadKey();
+        }
+    }
+
+}
